e1cec4b27334f40dcdabda28eaada990b7e9a990
xVesper <73543770+xVesper@users.noreply.github.com>
2021-01-07 16:25:37 +0100

    Update Zad5.sql

--- Projekt_Bazy/Zad5.sql
+++ Projekt_Bazy/Zad5.sql
@@ -1,10 +1,12 @@
-Funkacja;
+Funkacja oblicza zysk;
 CREATE FUNCTION calcProfit(cost FLOAT, price FLOAT) RETURNS DECIMAL(9,2) BEGIN DECLARE profit DECIMAL(9,2); SET profit = price-cost; RETURN profit; END
 
-Trigger;
+Trigger wpisuja w tabele kiedy ktos cos doda do tabeli klient;
 CREATE TRIGGER `insertLogi` AFTER INSERT ON `klient` FOR EACH ROW INSERT INTO logs VALUES(null, id,'Inserted',NOW());
-CREATE TRIGGER `UPDATEtLogi` AFTER UPDATE ON `klient` FOR EACH ROW INSERT INTO logs VALUES(null, id,'Instert',NOW())
+Trigger wpisuja w tabele kiedy ktos cos zmieni w tabeli klient;                                                                                         
+CREATE TRIGGER `UPDATEtLogi` AFTER UPDATE ON `klient` FOR EACH ROW INSERT INTO logs VALUES(null, id,'Instert',NOW());
 
-Procedury;
+Procedury znajdzie klienta po numerze telefonu;
 CREATE PROCEDURE `Klient`(IN `tel` INT(11)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT * FROM klient WHERE Nr_tel = tel; END
+Procedury znajdzie zamowienie po id;
 CREATE PROCEDURE `Zamowienie`(IN `pro` INT(11)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN SELECT * FROM zamowienie WHERE ID_Zamowienie = pro; END
